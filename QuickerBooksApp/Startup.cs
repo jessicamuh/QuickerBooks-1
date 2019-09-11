35050047435160ff00d22f1a17d48aeb8b4ac50f
Ayo Awodu <aawodu@students.kennesaw.edu>
2019-09-11 03:13:00 -0400

    Add project files.

---
+++ QuickerBooksApp/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using QuickerBooksApp.Models;
+
+[assembly: OwinStartupAttribute(typeof(QuickerBooksApp.Startup))]
+namespace QuickerBooksApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        //Create default User roles and Admin user for login   
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //Create Admin Role and default Admin User    
+            if (!roleManager.RoleExists("Administrator"))
+            {
+                // create Admin   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Administrator";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "aawodu0919";
+                user.Email = "ayomideawodu@yahoo.com";
+                string userPWD = "Momoland123!";
+
+                var checkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (checkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Administrator");
+                }
+            }
+
+            //Create Manager role    
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+            }
+
+            //Create regular user role    
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+
+        }
+
+
+
+    }
+}
